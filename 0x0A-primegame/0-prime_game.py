af09fe4b56e1471283788357a3da355e119c5f19
jaskieerr <nizar.zairat@gmail.com>
2024-12-12 11:06:15 -0800

    elif mrat sota hhhhhhhhhhhhhhhhhh

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-'''Prime Number Game'''
+"""Prime Number Game."""
 
 def find_primes(limit):
-    '''Generate a list of prime numbers up to a given limit'''
+    """Generate a list of prime numbers up to a given limit."""
     primes = []
     sieve = [True] * (limit + 1)
     for num in range(2, limit + 1):
@@ -12,19 +12,36 @@ def find_primes(limit):
                 sieve[multiple] = False
     return primes
 
-def determine_winner(rounds, numbers):
-    '''Determine the winner of the prime number game'''
-    if not rounds or not numbers:
+def isWinner(x, nums):
+    """Determine the overall winner after x rounds of the prime game."""
+    if not x or not nums:
         return None
 
     maria_score = 0
     ben_score = 0
 
-    for round_idx in range(rounds):
-        prime_list = find_primes(numbers[round_idx])
-        if len(prime_list) % 2 == 0:
+    for n in nums:
+        if n < 2:
             ben_score += 1
-        else:
+            continue
+
+        primes = find_primes(n)
+        prime_set = set(primes)
+        turn = 0  # 0 for Maria, 1 for Ben
+
+        while primes:
+            current_prime = primes.pop(0)
+            prime_set.discard(current_prime)
+            multiples = range(current_prime, n + 1, current_prime)
+            for m in multiples:
+                prime_set.discard(m)
+
+            primes = sorted(prime_set)
+            turn = 1 - turn
+
+        if turn == 1:  # Maria couldn't play, Ben wins
+            ben_score += 1
+        else:  # Ben couldn't play, Maria wins
             maria_score += 1
 
     if maria_score > ben_score:
