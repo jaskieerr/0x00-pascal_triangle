7876c87e3235a6d26fcacf9b112cb3faba4dfdb2
jaskieerr <nizar.zairat@gmail.com>
2024-12-12 11:13:42 -0800

    elif mrat sota hhhhhhhhhhhhhhhhhh

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,51 +1,34 @@
 #!/usr/bin/python3
-"""Prime Number Game."""
+'''Prime Number Game.'''
+
 
 def find_primes(limit):
-    """Generate a list of prime numbers up to a given limit."""
+    '''Generate a list of prime numbers up to a given limit'''
     primes = []
     sieve = [True] * (limit + 1)
-    for num in range(2, limit + 1):
-        if sieve[num]:
-            primes.append(num)
-            for multiple in range(num, limit + 1, num):
-                sieve[multiple] = False
+    for potentialPrime in range(2, limit + 1):
+        if sieve[potentialPrime]:
+            primes.append(potentialPrime)
+            for m in range(potentialPrime, limit + 1, potentialPrime):
+                sieve[m] = False
     return primes
 
-def isWinner(x, nums):
-    """Determine the overall winner after x rounds of the prime game."""
-    if not x or not nums:
+
+def victoriouso(numRounds, roundValues):
+    '''
+    Determine the victoriouso
+    '''
+    if not numRounds or not roundValues:
         return None
-
-    maria_score = 0
-    ben_score = 0
-
-    for n in nums:
-        if n < 2:
-            ben_score += 1
-            continue
-
-        primes = find_primes(n)
-        prime_set = set(primes)
-        turn = 0  # 0 for Maria, 1 for Ben
-
-        while primes:
-            current_prime = primes.pop(0)
-            prime_set.discard(current_prime)
-            multiples = range(current_prime, n + 1, current_prime)
-            for m in multiples:
-                prime_set.discard(m)
-
-            primes = sorted(prime_set)
-            turn = 1 - turn
-
-        if turn == 1:  # Maria couldn't play, Ben wins
-            ben_score += 1
-        else:  # Ben couldn't play, Maria wins
-            maria_score += 1
-
-    if maria_score > ben_score:
+    mariapts = benspts = 0
+    for ee in range(numRounds):
+        primes = find_primes(roundValues[ee])
+        if len(primes) % 2 == 0:
+            benspts += 1
+        else:
+            mariapts += 1
+    if mariapts > benspts:
         return "Maria"
-    elif ben_score > maria_score:
+    elif benspts > mariapts:
         return "Ben"
     return None
